af2e1b2a72d96500e82021f5e130c7c15986cc26
HARSH-dev1708 <harshk0804@gmail.com>
2026-05-30 22:42:22 +0530

    Flash buttons and level up

--- app.js
+++ app.js
@@ -4,9 +4,36 @@ let userSeq = [];
 let level = 0;
 let started = false;
 
+let btns = ["yellow", "red", "green", "blue"];
+
+let h2 = document.querySelector("h2");
+
 document.addEventListener("keypress", function () {
     if(started == false){
         console.log("Game started");
         started = true;
+
+        levelUp();
     }
 });
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    }, 300);
+}
+
+
+function levelUp(){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*4);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    btnFlash(randBtn);
+}
