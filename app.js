2e43bc94b0ee85d3a5a4f168c5fbac2014a19163
HARSH-dev1708 <harshk0804@gmail.com>
2026-05-30 23:00:45 +0530

    Add button event listeners

--- app.js
+++ app.js
@@ -17,12 +17,20 @@ document.addEventListener("keypress", function () {
     }
 });
 
-function btnFlash(btn){
+function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function () {
         btn.classList.remove("flash");
     }, 300);
 }
+
+function userFlash(btn){
+    btn.classList.add("user-flash");
+    setTimeout(function () {
+        btn.classList.remove("user-flash");
+    }, 300);
+}
+
 
 
 function levelUp(){
@@ -35,5 +43,17 @@ function levelUp(){
     console.log(randIdx);
     console.log(randColor);
     console.log(randBtn);
-    btnFlash(randBtn);
+    gameFlash(randBtn);
+}
+
+function btnPress(){
+    console.log(this.classList);
+    let btn = this;
+    userFlash(btn);
+}
+
+let allBtns =  document.querySelectorAll(".btn");
+
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
 }
